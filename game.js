3f57ddca6a1df0f03fe2a0672606e88904d5faf7
WilmaNyren <nywi24wv@student.ju.se>
2024-11-13 15:23:39 +0100

    animation

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-let angelY = 50;
-let angelX = 275;
+let y = 50;
+let x = 200;
 
 let velocityY = 0.5;
 let acceleration = 0.5;
@@ -13,9 +13,10 @@ function setup() {
 function angel(x, y) {
   //to shange scale of the angel
   push();
-  scale(0.8);
+  scale(1);
   //wings rhigt
   stroke(100);
+  strokeWeight(3);
   fill(255);
   push();
   translate(x + 100, y + 71);
@@ -77,8 +78,77 @@ function angel(x, y) {
   pop();
 }
 
+function angelWingDown(x, y) {
+  //to shange scale of the angel
+  push();
+  scale(1);
+  //wings rhigt
+  stroke(100);
+  strokeWeight(3);
+  fill(255);
+  push();
+  translate(x + 120, y + 71);
+  rotate(0.45);
+  beginShape();
+  vertex(-5, 24);
+  bezierVertex(0, 30, 65, 40, 65, -20);
+  line(-3, 22, 64, -21);
+  endShape();
+  pop();
+
+  //wing left
+  push();
+  translate(x + 30, y + 65);
+  rotate(0.7);
+  beginShape();
+  vertex(-5, 24);
+  bezierVertex(0, 30, 65, 40, 65, -20);
+  line(-5, 23, 64, -21);
+  endShape();
+  pop();
+
+  //halo
+  strokeWeight(6);
+  stroke(255, 234, 99);
+  noFill();
+  ellipse(x + 100, y + 20, 50, 20);
+
+  //extras
+  stroke(100);
+  strokeWeight(3);
+
+  //legs
+  line(x + 80, y + 140, x + 60, y + 165);
+  line(x + 80, y + 160, x + 60, y + 165);
+  //--------
+  line(x + 120, y + 140, x + 140, y + 165);
+  line(x + 140, y + 165, x + 120, y + 160);
+
+  // arms
+  line(x + 100, y + 85, x + 160, y + 40);
+  line(x + 100, y + 85, x + 40, y + 40);
+
+  //body
+  fill(255);
+  triangle(x + 60, y + 140, x + 140, y + 140, x + 100, y + 80);
+
+  //head
+  ellipse(x + 100, y + 50, 60);
+
+  //face
+  stroke(199, 18, 17);
+  fill(255);
+  rect(x + 87, y + 60, 26);
+  stroke(0);
+  fill(0);
+  ellipse(x + 85, y + 40, 2);
+  ellipse(x + 115, y + 40, 2);
+  pop();
+}
+
 //game-over scren
 function gameOverScreen() {
+  background(153, 179, 255);
   stroke(255);
   strokeWeight(5);
   fill(200, 50, 50);
@@ -87,29 +157,45 @@ function gameOverScreen() {
   textSize(60);
   fill(255);
   text("Game Over", 150, 260, 500);
+  //button
+  stroke(255);
+  strokeWeight(4);
+  fill(200, 50, 50);
+  rect(190, 420, 220, 50, 10);
+  fill(255);
+  textSize(30);
+  strokeWeight(1);
+  text("Play Again", 228, 430, 500, 100);
 }
 
 function draw() {
   background(153, 211, 255);
-  angel(angelX, angelY);
+  angel(x, y);
   let gameState = "true";
 
   if (gameState === "true") {
-    angelY = angelY + velocityY;
+    y = y + velocityY;
     velocityY = velocityY + acceleration;
   }
   if (keyIsDown(38)) {
     velocityY = velocityY - 2;
+    clear ();
+    background(153, 211, 255);
+    angelWingDown (x,y);
+    
+    
   }
+  
 
   //game over
-  if (angelY > 650) {
+  if (y >= 715) {
     gameState = false;
     console.log("game over");
     gameOverScreen();
-    
+    // make button later
   }
 }
+
 
 
 
